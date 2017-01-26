0b83d6b4cb795c58a365625fbd39dfd606dc4550
Daouda Sangare <dsangare@kippnyccp.org>
2017-01-26 15:08:48 -0700

    Daouda Sangare, January 26, 2017 Project

--- prj/Driver_prj0.java
+++ prj/Driver_prj0.java
@@ -1,5 +1,15 @@
-public class Driver_prj0 {
+/**
+  * file: Driver_prj0.java 
+  * author: Daouda Sangare
+  * course: CMPT220
+  * assignment: lab 1
+  * due date: Thursday January 26, 2017
+  * Version: 2
+  *
+  * This file contains my code for work for Project 0
+  */
+  public class Driver_prj0 {
   public static void main(String[] args) {
-    System.out.printIn("Hello World!")
+    System.out.println("Hello World!");
   }
 }     	  
